d51e2ad16465b47e4ea6abb3add46b8e94438826
O Rabie <o.rabie@tap.company>
2020-09-22 11:41:53 +0200

    Upgrade to Xcode12+ compiler friendly code

---
+++ TapBundleLocalizationV2/TapBundleLocalizationV2/TapBundleLocalizationV2.h
@@ -0,0 +1,18 @@
+//
+//  TapBundleLocalizationV2.h
+//  TapBundleLocalizationV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapBundleLocalizationV2.
+FOUNDATION_EXPORT double TapBundleLocalizationV2VersionNumber;
+
+//! Project version string for TapBundleLocalizationV2.
+FOUNDATION_EXPORT const unsigned char TapBundleLocalizationV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapBundleLocalizationV2/PublicHeader.h>
+
+
